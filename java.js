88aa58b96d083ce8b292091303e836bd25d2236d
subbareddybijivemula <121100423+subbareddybijivemula@users.noreply.github.com>
2022-12-29 12:11:35 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<script>
+function abc()
+{
+   document.get ElementByid("ab").innerHTML ="LPU excepts some better placements out of you:;
+}
+</script>
+</head>
+<body>
+<p id="ab">Welcome to LPU</P>
+<input type="submit" onclick="abc()">
+</body>
+</html>
